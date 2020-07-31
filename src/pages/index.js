35ed0d99448eb5fe5659a955b9426d809b1f0200
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-01 02:31:58 +0700

    move content folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,35 @@
-import fs from 'fs'
-import matter from 'gray-matter'
-import glob from 'fast-glob'
+import fs from "fs";
+import matter from "gray-matter";
+import glob from "fast-glob";
 
-import { HomePage } from 'components'
+import { HomePage } from "components";
 
-export default HomePage
+export default HomePage;
 
 export async function getStaticProps() {
-  const files = glob.sync('src/content/**/*.mdx')
+  const files = glob.sync("content/**/*.mdx");
 
-  const allMdx = files.map(file => {
-    const split = file.split('/')
-    const filename = split[split.length - 1]
-    const slug = filename.replace('.mdx', '')
+  const allMdx = files.map((file) => {
+    const split = file.split("/");
+    const filename = split[split.length - 1];
+    const slug = filename.replace(".mdx", "");
 
-    const mdxSource = fs.readFileSync(file)
-    const { data } = matter(mdxSource)
+    const mdxSource = fs.readFileSync(file);
+    const { data } = matter(mdxSource);
 
     return {
       slug,
-      frontMatter: data
-    }
-  })
+      frontMatter: data,
+    };
+  });
 
   const orderedByDate = allMdx.sort((a, b) => {
-    return new Date(b.frontMatter.date) - new Date(a.frontMatter.date)
-  })
+    return new Date(b.frontMatter.date) - new Date(a.frontMatter.date);
+  });
 
   return {
     props: {
-      allMdx: orderedByDate
-    }
-  }
+      allMdx: orderedByDate,
+    },
+  };
 }
