661856bf043335f0223057d132d8dd8b00ec2e98
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-05 02:16:57 +0700

    add list of tags to Posts index page

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,16 +1,17 @@
-import { getAllPosts } from "../../utils/get-mdx";
-
+import Link from "next/link";
 import { PostSnippet } from "components";
+import { getAllPosts, getAllTags } from "../../utils/get-mdx";
 
 export async function getStaticProps() {
   return {
     props: {
       allMdx: getAllPosts("posts", "slug"),
+      allTags: getAllTags("posts"),
     },
   };
 }
 
-export default function Posts({ allMdx }) {
+export default function Posts({ allMdx, allTags }) {
   return (
     <main>
       <h1>Posts</h1>
@@ -19,6 +20,19 @@ export default function Posts({ allMdx }) {
           {allMdx.map((item) => (
             <li key={item.slug}>
               <PostSnippet {...item} />
+            </li>
+          ))}
+        </ul>
+      )}
+      <hr />
+      <h2>Post Tags</h2>
+      {allTags && (
+        <ul>
+          {allTags.map((tag) => (
+            <li key={tag} sx={{ ml: 2 }}>
+              <Link href="/posts/tags/[tag]" as={`/posts/tags/${tag}`} passHref>
+                <a>{`#${tag}`}</a>
+              </Link>
             </li>
           ))}
         </ul>
