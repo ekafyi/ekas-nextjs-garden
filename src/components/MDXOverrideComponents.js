679cd5b7abfb715a7f8041bb74e23029b0c82dde
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-05 00:39:55 +0700

    try next-mdx-remote

--- src/components/MDXOverrideComponents.js
+++ src/components/MDXOverrideComponents.js
@@ -1,7 +1,9 @@
+// TODO replace with theme-ui way
 export const p = ({ children }) => {
   return (
     <p
       style={{
+        background: "hotpink",
         fontSize: "18px",
         lineHeight: "180%",
         marginBottom: "3rem",
--- src/pages/posts/[slug].js
+++ src/pages/posts/[slug].js
@@ -1,11 +1,23 @@
-import fs from "fs";
-import MDX from "@mdx-js/runtime";
-import ReactDOM from "react-dom/server";
+import renderToString from "next-mdx-remote/render-to-string";
+import hydrate from "next-mdx-remote/hydrate";
 import matter from "gray-matter";
 import glob from "fast-glob";
-import { getContentGlob, getSlug } from "../../utils/get-mdx";
+import { getContentGlob, getContentPath, getSlug } from "../../utils/get-mdx";
+
+import fs from "fs";
+import path from "path";
 
 import * as components from "components";
+
+export default function TestPage({ mdxSource, frontMatter }) {
+  const content = hydrate(mdxSource, components);
+  return (
+    <div>
+      <h1>{frontMatter.title}</h1>
+      {content}
+    </div>
+  );
+}
 
 export async function getStaticPaths() {
   const files = glob.sync(getContentGlob("posts")); // only generate paths from 'content/posts'
@@ -23,27 +35,18 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params: { slug } }) {
-  const files = glob.sync(getContentGlob("posts")); // only look in 'content/posts'
+  // from https://github.com/hashicorp/next-mdx-remote
 
-  const fullPath = files.filter((item) => {
-    return getSlug(item) === slug;
-  })[0];
+  // prettier-ignore
+  const foo = path.join(path.join(process.cwd()), `${getContentPath("posts")}/${slug}.mdx`);
+  const source = fs.readFileSync(foo);
 
-  const mdxSource = fs.readFileSync(fullPath);
-  const { content, data } = matter(mdxSource);
-
-  if (!fullPath) {
-    console.warn("No MDX file found for slug");
-  }
-
+  const { content, data } = matter(source);
+  const mdxSource = await renderToString(content, components, null, data);
   return {
     props: {
-      mdxHtml: ReactDOM.renderToStaticMarkup(
-        <MDX components={components}>{content}</MDX>
-      ),
-      frontMatter: data || {},
+      mdxSource,
+      frontMatter: data,
     },
   };
 }
-
-export default components.PostPage;
