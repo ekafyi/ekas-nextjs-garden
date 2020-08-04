07f5e4c68630811741014bdec7da8788c38c867c
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-04 22:28:20 +0700

    add tags to PostSnippet

--- src/components/PostSnippet.js
+++ src/components/PostSnippet.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
-import Link from 'next/link'
+import Link from "next/link";
 
-export default function PostSnippet ({ slug, dynHref, frontMatter }) {
+export default function PostSnippet({ slug, dynHref, frontMatter }) {
   return (
     <>
       <Link href={dynHref} as={slug} prefetch={false} passHref>
@@ -11,10 +11,26 @@ export default function PostSnippet ({ slug, dynHref, frontMatter }) {
       <div
         sx={{
           mt: 2,
+          display: "flex",
           variant: "text.date.snippet",
         }}
       >
-        {frontMatter.date}
+        <span>{frontMatter.date}</span>
+        {frontMatter.tags && (
+          <ul sx={{ display: "inline-flex" }}>
+            {frontMatter.tags.map((tag) => (
+              <li key={tag} sx={{ ml: 2 }}>
+                <Link
+                  href="/posts/tags/[tag]"
+                  as={`/posts/tags/${tag}`}
+                  passHref
+                >
+                  <a>{`#${tag}`}</a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </>
   );
