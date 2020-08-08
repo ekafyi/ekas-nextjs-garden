2146c74a115efff84327e747ce4987415333da7c
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-09 01:44:43 +0700

    fix jsx attribute SEO

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -61,7 +61,7 @@ export default function SEO({
 
       {/* Don't change these. */}
       <meta charSet="utf-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
   );
