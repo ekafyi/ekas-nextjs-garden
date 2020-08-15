1873acee710332398334badf290c175c725dd2d9
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-15 20:16:43 +0700

    try next-mdx-remote (again)

--- src/components/MDXComponents.js
+++ src/components/MDXComponents.js
@@ -1,8 +1,8 @@
 import { Styled } from "theme-ui";
 
-// export const p = ({ children }) => {
-//   return <Styled.p>{children}</Styled.p>;
-// };
+export const p = ({ children }) => {
+  return <Styled.p>{children}</Styled.p>;
+};
 
 // export const h2 = ({ children }) => {
 //   return <Styled.h2>{children}</Styled.h2>;
