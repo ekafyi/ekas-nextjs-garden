bd99a72f81f292bd14b3d05d01b2c1a331a5da49
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-17 04:15:49 +0700

    last try on mdx components stuff

--- src/pages/notes/[slug].js
+++ src/pages/notes/[slug].js
@@ -2,15 +2,15 @@ import renderToString from "next-mdx-remote/render-to-string";
 import { getAllSlugsStaticPaths, getPost } from "../../utils/get-mdx";
 import { NotePage } from "components";
 
-import dynamic from "next/dynamic";
-import mdxComponents from "components/mdx";
+// import dynamic from "next/dynamic";
+// import mdxComponents from "components/mdx";
 
-const CodeBlock = dynamic(() => import("../../components/mdx/CodeBlock")); // It's somehow faster when imported here vs from components/mdx 🤔.
-const components = {
-  pre: ({ children }) => <>{children}</>,
-  code: CodeBlock,
-  ...mdxComponents,
-};
+// const CodeBlock = dynamic(() => import("../../components/mdx/CodeBlock")); // It's somehow faster when imported here vs from components/mdx 🤔.
+// const components = {
+//   pre: ({ children }) => <>{children}</>,
+//   code: CodeBlock,
+//   ...mdxComponents,
+// };
 
 export async function getStaticPaths() {
   const paths = getAllSlugsStaticPaths("notes");
@@ -23,7 +23,7 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
   const post = getPost(slug, "notes");
   if (!post) return { props: { mdxContent: null, frontMatter: {} } };
-  const mdxContent = await renderToString(post.mdx, components);
+  const mdxContent = await renderToString(post.mdx);
   return {
     props: {
       mdxContent,
