4366c19f12da6584b9c78c0ee7364bb00ae19c2c
Eka <6597211+ekafyi@users.noreply.github.com>
2020-08-18 21:42:07 +0700

    add progress loader

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,11 +3,13 @@ import NextApp from "next/app";
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
 import "../utils/tailwind.css";
+import NextNProgress from "nextjs-progressbar";
 export default class App extends NextApp {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <ThemeProvider theme={theme}>
+        <NextNProgress color="currentColor" height={2} />
         <Component {...pageProps} />
       </ThemeProvider>
     );
