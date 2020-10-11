91ced8fed480807127c04da5c4602a9400cfdbd1
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-11 15:08:42 +0700

    update SEO based on new taxonomies

--- src/utils/note-utils.js
+++ src/utils/note-utils.js
@@ -1,3 +1,5 @@
+import { tags as tagsConfig } from "../../taxonomies.yml";
+
 export const getNoteDesc = (excerpt, tags = [], techs = []) => {
   let desc = "My digital garden note.";
 
@@ -6,13 +8,13 @@ export const getNoteDesc = (excerpt, tags = [], techs = []) => {
       // "My digital garden note on …"
       desc = `My digital garden note on ${techs.join(", ")}`;
       if (tags.length) {
-        if (tags[0] === "TIPS") {
+        if (tags[0] === tagsConfig[0].name) {
           // "Coding/development note on ..."
           desc = `Coding/development note on ${techs.join(", ")}`;
-        } else if (tags[0] === "LEARNING_NOTES") {
+        } else if (tags[0] === tagsConfig[1].name) {
           // "Learning note on ..."
           desc = `Learning note on ${techs.join(", ")}`;
-        } else if (tags[0] === "RESOURCES") {
+        } else if (tags[0] === tagsConfig[2].name) {
           // "Resources on ..."
           desc = `Resources on ${techs.join(", ")}`;
         }
@@ -21,11 +23,11 @@ export const getNoteDesc = (excerpt, tags = [], techs = []) => {
       // "My digital garden note tagged …"
       desc = `My digital garden note tagged ${tags.join(", ")}`;
       if (tags.length > 1) {
-        if (tags[0] === "TIPS") {
+        if (tags[0] === tagsConfig[0].name) {
           desc = `Coding/development note tagged ${tags.join(", ")}`;
-        } else if (tags[0] === "LEARNING_NOTES") {
+        } else if (tags[0] === tagsConfig[1].name) {
           desc = `Learning note tagged ${tags.join(", ")}`;
-        } else if (tags[0] === "RESOURCES") {
+        } else if (tags[0] === tagsConfig[2].name) {
           desc = `Resources tagged ${tags.join(", ")}`;
         }
       }
