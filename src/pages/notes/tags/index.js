615e0ae13d3348ffa5826fcffb761c3a19fd33b9
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-23 20:52:46 +0700

    style tags index page

--- src/pages/notes/tags/index.js
+++ src/pages/notes/tags/index.js
@@ -1,6 +1,16 @@
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { SEO, SkipLink, Nav } from "components";
 import { getAllTags } from "src/utils/get-mdx";
 import { getTagFriendlyName } from "src/utils/get-taxonomy";
+
+const TAGLIST_SX = { display: "flex", flexWrap: "wrap" };
+
+const TAG_SX = { variant: "buttons.pill", fontSize: 4, mr: 3, mb: 2 };
+
+const H_SX = { variant: "text.pageHeading", my: [8, null, 12] };
 
 export async function getStaticProps() {
   return {
@@ -11,27 +21,33 @@ export async function getStaticProps() {
 }
 
 export default function Tags({ allTags }) {
+  const router = useRouter();
   return (
-    <main>
-      <h1 className="sr-only">Notes Tags</h1>
-      {allTags && (
-        <ul>
-          {allTags.map((tag) => {
-            const tagLabel = getTagFriendlyName(tag);
-            return (
-              <li key={tag} sx={{ ml: 2 }}>
-                <Link
-                  href="/notes/tags/[tag]"
-                  as={`/notes/tags/${tagLabel}`}
-                  passHref
-                >
-                  <a>{tagLabel}</a>
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      )}
-    </main>
+    <>
+      <SEO title="Tags Archive" path={router.asPath} />
+      <SkipLink />
+      <main sx={{ variant: "layout.container" }}>
+        <Nav curPath={router.asPath} />
+        <h1 sx={H_SX}>Tags</h1>
+        {allTags && (
+          <ul id="main" sx={TAGLIST_SX}>
+            {allTags.map((tag) => {
+              const tagLabel = getTagFriendlyName(tag);
+              return (
+                <li key={tag}>
+                  <Link
+                    href="/notes/tags/[tag]"
+                    as={`/notes/tags/${tagLabel}`}
+                    passHref
+                  >
+                    <a sx={TAG_SX}>{tagLabel}</a>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        )}
+      </main>
+    </>
   );
 }
