0c4e7b4d2a2706ca16e3d19ca68d387dc1548f9c
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-23 22:15:21 +0700

    modify next config to use ES6

--- next.config.js
+++ next.config.js
@@ -11,7 +11,8 @@ const withMDX = require("@next/mdx")({
 
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "md", "mdx"],
-  webpack(config) {
+
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.node = {
       fs: "empty",
     };
