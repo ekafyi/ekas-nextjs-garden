61b4259abe786f239439ac843504a5390091308e
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-25 00:43:45 +0700

    huh

--- src/components/mdx/Image.js
+++ src/components/mdx/Image.js
@@ -15,7 +15,7 @@ export default function Image({ src, alt, figcaption, ...props }) {
       <LazyLoadImage
         src={src}
         alt={alt || ""}
-        threshold={100}
+        threshold={0}
         effect="opacity"
         afterLoad={() => setHasLoaded(true)}
         {...props}
