765bb1716066273c84104a5aa44a5c5dfc618bfb
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-29 20:08:20 +0700

    add hygen generator for new basic component

---
+++ src/components/Menu.js
@@ -0,0 +1,10 @@
+
+/** @jsx jsx */
+import { jsx } from "theme-ui";
+//import { useState, useEffect } from "react";
+
+export default function Menu({ ...props }) {
+  return (
+    <div>hello component</div>
+  );
+}
