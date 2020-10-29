29d993babb8f2035a26ebefa64ddf11a962eea64
Eka <6597211+ekafyi@users.noreply.github.com>
2020-10-29 15:16:49 +0700

    remove microblog

--- src/utils/get-components.js
+++ src/utils/get-components.js
@@ -1,8 +1,15 @@
 import * as components from "components";
 
 /**
- * Components to pass to markdown-to-jsx.
- * Example: pages/microblog/index.js
+ * Components to pass to markdown-to-jsx. CURRENTLY NOT USED.
+ * 
+ * Usage:
+
+import Markdown from "markdown-to-jsx";
+
+<Markdown options={{ overrides: mdToJsxComponents }}>
+  {mdxBody}
+</Markdown>
  */
 export const mdToJsxComponents = {
   Mug: {
